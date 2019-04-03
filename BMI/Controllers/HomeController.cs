d4df305a78e09b86acb782f882b1f4d06334c49a
Aakanksha Goyal <Aakanksha.Goyal@qnrl.com>
2019-04-03 11:12:38 +0100

    Upload

--- BMI/Controllers/HomeController.cs
+++ BMI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BMI.Models;
+using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,38 @@ namespace BMI.Controllers
 {
     public class HomeController : Controller
     {
+        
+        public ActionResult Upload(FormCollection formCollection)
+        {
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))   
+        {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    var usersList = new List<BMIUsers>();
+                    
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)   
+                {
+                            var user = new BMIUsers();
+                            user.FirstName = workSheet.Cells[rowIterator, 1].Value.ToString();
+                            user.LastName = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            usersList.Add(user);
+                        }
+                    }
+                }
+            }
+            return View("Index");
+        }
         public ActionResult Index()
         {
             return View();
---
+++ BMI/Models/BMIUsers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BMI.Models
+{
+    public class BMIUsers
+    {
+        
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        
+    }
+}
